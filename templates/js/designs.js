39ff50c021584a261913f45c7e663fc62507028f
Douglasz <stevenzenne@gmail.com>
2017-10-22 18:29:04 +0100

    remove carousel from recipe catalog and use jumbotrpn

--- templates/js/designs.js
+++ templates/js/designs.js
@@ -44,3 +44,16 @@
 // // Cycles to the previous item.
 
 // $('.carousel').carousel('next')
+
+// Activate Carousel
+$("#myCarousel").carousel();
+
+// Enable Carousel Indicators
+$(".item").click(function(){
+    $("#myCarousel").carousel(1);
+});
+
+// Enable Carousel Controls
+$(".left").click(function(){
+    $("#myCarousel").carousel("prev");
+});
